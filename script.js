a9f2eac75033c85a68c8610646b5c278020e5bd4
Mindaugas Jančaras <minius17@gmail.com>
2021-07-24 20:08:31 +0300

    finish

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const pwEL = document.getElementById("pw");
-const copyEL = document.getElementById("copy");
-const lenEL = document.getElementById("len");
-const upperEL = document.getElementById("upper");
-const lowerEL = document.getElementById("lower");
-const numberEL = document.getElementById("number");
-const symbolEL = document.getElementById("symbol");
-const generateEL = document.getElementById("generate");
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
 
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
-const symbols = "!@#$%^&*()_+=-][{}.,/";
+const symbols = "!@#$%^&*()_+=";
 
 function getLowercase() {
-return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
 }
 
 function getUppercase() {
@@ -21,45 +21,79 @@ function getUppercase() {
 }
 
 function getNumber() {
-    return numbers[Math.floor(Math.random() * numbers.length)]; 
+    return numbers[Math.floor(Math.random() * numbers.length)];
 }
 
 function getSymbol() {
-    return symbols[Math.floor(Math.random() * symbols.length)]; 
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-
 function generatePassword() {
-  const len = lenEL.nodeValue;
+    const len = lenEl.value;
 
-  let password ='';
-  
-  for(let i=0; i<len; i++){
-      const x = generateX();
-    password += x;
-  }
-  
-    pwEL.innerText = password;  
+    let password = "";
+
+    if (upperEl.checked) {
+        password += getUppercase();
+    }
+
+    if (lowerEl.checked) {
+        password += getLowercase();
+    }
+
+    if (numberEl.checked) {
+        password += getNumber();
+    }
+
+    if (symbolEl.checked) {
+        password += getSymbol();
+    }
+
+    for (let i = password.length; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
 }
 
 function generateX() {
-   const xs = [];
-   if (upperEl.checked) {
-       xs.push(getUppercase());
-   }
-   
-   if (lowerEl.checked) {
-    xs.push(getLowercase());
-}
-if (numberEl.checked) {
-    xs.push(getNumber());
-}
-if (symbolEl.checked) {
-    xs.push(getSymbol());
+    const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+
+    if (xs.length === 0) return "";
+
+    return xs[Math.floor(Math.random() * xs.length)];
 }
 
-return xs[Math.floor(Math.random() * xs.length)];
+generateEl.addEventListener("click", generatePassword);
 
-}
+copyEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const password = pwEl.innerText;
 
-generateEL.addEventListener('click', generatePassword);
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard");
+});
